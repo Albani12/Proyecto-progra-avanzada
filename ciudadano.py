e0f53b8df92110d94d427b33e335ecd7783d71fa
Albani12 <albanimedina_11@hotmail.com>
2024-07-09 01:33:20 -0400

    Modelo SIR
    Se mejora la implementacion del modelo sir, de modo que se tienen metodos para infectar, recuperar infectar, pero se anade el morir, asi tambien un contador de pasos de infeccion para seguir el progreso de la infeccion, lo cual es esencial para controlar el tiempo que un individuo está en estado infectado y aplicar las probabilidades de recuperacion o muerte correctamente

--- ciudadano.py
+++ ciudadano.py
@@ -1,17 +1,18 @@
 import numpy as np 
 import random
+from enfermedad import Enfermedad
 
 class Ciudadano():  #Representa a cada individuo en la simulación
-    def __init__(self, id, nombre, apellido, comunidad, familia, enfermedad, estado, gamma):
+    def __init__(self, id, nombre, apellido, comunidad, familia, enfermedad, estado, gamma=0.5):
         self.__id = id 
         self.__nombre = nombre 
         self.__apellido = apellido 
         self.__comunidad = comunidad 
         self.__familia = familia 
         self.__enfermedad = enfermedad
-        self.__estado = estado  #'S': Susceptible, 'I': Infectado, 'R': Recuperado
-        self.__gamma = gamma
-        self.__contador = 0
+        self.__estado = estado  #'S': Susceptible, 'I': Infectado, 'R': Recuperado, Se agrega M, cuando muere
+        self.__gamma = gamma #Tasa de recuperación
+        self.__contador_pasos = 0
 
     #Set y get para atributo privado de la clase
 
@@ -57,34 +58,55 @@ class Ciudadano():  #Representa a cada individuo en la simulación
     def set_estado(self, estado):
         self.__estado = estado
 
-    def get_contador(self):
-        return self.__contador
+    def get_contador_pasos(self):
+        return self.__contador_pasos
 
-    def set_contador(self, contador):
-        self.__contador = contador
-    
-    def infectar(self):
-        self.__estado = 'I'
-        self.__contador = 0
-
-    def recuperar(self):
-        self.__estado = 'R'
-        self.__contador = 0
+    def set_contador_pasos(self, contador_pasos):
+        self.__contador_pasos = contador_pasos
 
     def get_gamma(self):
         return self.__gamma
 
     def set_gamma(self, gamma):
         self.__gamma = gamma
+    
+    def infectar(self):
+        self.__estado = 'I'
+        self.__contador_pasos = 0
 
-    def paso(self, gamma): #Gamma es la tasa de recuperacion
+    def recuperar(self):
+        self.__estado = 'R'
+        self.__contador_pasos = 0
+
+    def morir(self):
         if self.__estado == 'I':
-            self.__contador += 1
-            if np.random.random() < self.__gamma:  #probabilidad gamma de que el individuo se recupere
+            self.__estado = 'M'  # Indicar estado de muerte
+
+    def paso(self):
+        if self.__estado == 'I':
+            self.__contador_pasos += 1
+            if np.random.normal() < self.__enfermedad.get_gamma():   #probabilidad gamma de que el individuo se recupere
                 self.recuperar()
-            #sugerencia del profe: usar random de numpy
+            elif np.random.normal() < self.__enfermedad.get_probabilidad_muerte():
+                self.morir()
+
+    def actualizar_estado(self):
+        if self.__estado == 'I':
+            self.__contador_pasos += 1
+            if self.__contador_pasos >= self.__enfermedad.tiempo_recuperacion():
+                if np.random.normal() < self.__enfermedad.get_probabilidad_recuperacion():
+                    self.recuperar()
+                elif np.random.normal() < self.__enfermedad.get_probabilidad_muerte():
+                    self.morir()
+            else:
+                for otro in self.__familia:
+                    if otro.get_estado() == 'S' and np.random.normal() < self.__enfermedad.get_infeccion_probable():
+                        otro.infectar()
+    #sugerencia del profe: usar random de numpy
 
     def representacion(self):
-        return f"{self.get_nombre} {self.get_apellido}: {self.get_estado}"
+        return f"{self.__nombre} {self.__apellido}: {self.__estado}"
     
+
+
     
